31232c6103d730df4be82c55d5c695c41f2727f8
realxkx-stack <realxkx@outlook.com>
2026-01-05 17:58:33 +0900

    update userの内容

--- src/main/java/com/device/management/dto/UserDTO.java
+++ src/main/java/com/device/management/dto/UserDTO.java
@@ -7,16 +7,18 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 /**
- * 用户 DTO
+ * user DTO
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-    private String userId;
-    private String userName;
-    private String departmentCode;
-    private String userLevel;
-    private LocalDateTime createdDate;
-    private LocalDateTime updatedDate;
+    private String userId; //ユーザID
+    private String deptId; //部署番号
+    private String name; //氏名
+    private Long userTypeId; //ユーザタイプID（辞書項目：USER_TYPE 関連）
+    private LocalDateTime createTime; //作成日時
+    private String creater; //作成者
+    private LocalDateTime updateTime; //更新日時
+    private String updater; //更新者
 }
