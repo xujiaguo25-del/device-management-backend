9939be20559770daaccf29855b3cac5983535b61
slkxyo <slkxyo@outlook.com>
2026-01-09 10:20:03 +0800

    エクスポートコードの完了

--- src/main/java/com/device/management/entity/SamplingCheck.java
+++ src/main/java/com/device/management/entity/SamplingCheck.java
@@ -2,7 +2,6 @@ package com.device.management.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
