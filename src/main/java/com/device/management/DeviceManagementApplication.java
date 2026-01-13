9ab744fcf3e0e86a32377e11cb7d3f0be2e55f06
Xu-Kangxing <realxkx@outlook.com>
2026-01-13 21:19:11 +0800

    Database roll back

--- src/main/java/com/device/management/DeviceManagementApplication.java
+++ src/main/java/com/device/management/DeviceManagementApplication.java
@@ -1,10 +1,7 @@
 package com.device.management;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 /**
  * 设备管理系统 Spring Boot 主应用程序
  */
